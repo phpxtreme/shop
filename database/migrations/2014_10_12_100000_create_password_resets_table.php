43325ab2ec91b7993295b75d8facf62ffd5aca08
Leonardo Yanes Batista <leo.phpxtreme@gmail.com>
2018-07-28 14:23:45 -0400

    #1 Fix in the migration for the table 'password_resets'

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -1,11 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreatePasswordResetsTable extends Migration
 {
+    /**
+     * Password Resets Table
+     *
+     * @var string
+     */
+    private $table = 'password_resets';
+
     /**
      * Run the migrations.
      *
@@ -13,7 +20,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
+        Schema::create($this->table, function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
@@ -27,6 +34,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists($this->table);
     }
 }
