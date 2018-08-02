1427a2405e345298d0b05063541f227895efe5d2
Leonardo Yanes Batista <leo.phpxtreme@gmail.com>
2018-08-02 01:31:48 -0400

    #1 Working on the structure of the wrap file of the views

--- resources/views/wrapper.blade.php
+++ resources/views/wrapper.blade.php
@@ -1,45 +1,62 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title></title>
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        @yield('styles')
-    </head>
-    <body>
-        <nav class="navbar navbar-custom navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <div class="container">
-                <a href="" class="navbar-brand">Shop!</a>
-                <button class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a href="" class="nav-link">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="" class="nav-link">Shop</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="" class="nav-link">
-                                <i class="fa fa-shopping-cart"></i>
-                                <span class="badge badge-pill badge-light">0</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('styles')
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <div class="container">
+            <a href="{{ route('page.home') }}" class="navbar-brand">Shop!</a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Shop</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-search"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-shopping-cart"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-        <header>
-            <div class="container text-center">
-                <h1>Welcome to our Shop!</h1>
-            </div>
-        </header>
-        @yield('content')
-        @yield('scripts')
-        <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
-    </body>
+        </div>
+    </nav>
+    <header>
+        <div class="jumbotron jumbotron-fluid text-center">
+            <h1>Welcome to our Shop!</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire...</p>
+        </div>
+    </header>
+    <div class="container">
+        <div class="row">
+            @yield('content')
+        </div>
+    </div>
+    <footer class="py-5 bg-dark">
+        <div class="container"></div>
+    </footer>
+    @yield('scripts')
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
 </html>
